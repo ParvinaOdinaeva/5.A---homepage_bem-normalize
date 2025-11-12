9952974bea2568127b5163ad5991d4064475b6b8
Parvina Odinaeva <odinaeva.parvina@gmail.com>
2025-11-12 14:56:16 +0100

    function modification

--- js/script.js
+++ js/script.js
@@ -1,32 +1,41 @@
 {
-  let questions = document.querySelector(".questions");
-  let questionsButton = document.querySelector(".questionsButton");
-  let questionsChangeWord = document.querySelector(".questionsChangeWord");
+  const hidingAnElement = () => {
+    let questions = document.querySelector(".questions");
+    let questionsChangeWord = document.querySelector(".questionsChangeWord");
 
-  questionsButton.addEventListener("click", () => {
     questions.classList.toggle("questinsHideContainer");
     questionsChangeWord.innerText = questions.classList.contains(
       "questinsHideContainer"
     )
       ? "Pokaż"
       : "Ukryj";
-  });
+  };
+
+  const init = () => {
+    let questionsButton = document.querySelector(".questionsButton");
+    questionsButton.addEventListener("click", hidingAnElement);
+  };
+
+  init();
 }
 
 {
-  let backgroundButton = document.querySelector(".backgroundButton");
-  let backgroundChangeWord = document.querySelector(".backgroundChangeWord");
-  let mainContainerBackground = document.querySelector(
-    ".mainContainerBackground"
-  );
+   const onBackgroundChangeButton = () => {
+    let backgroundChangeWord = document.querySelector(".backgroundChangeWord");
+    let mainContainerBackground = document.querySelector(".mainContainerBackground");
 
-  backgroundButton.addEventListener("click", () => {
     mainContainerBackground.classList.toggle("mainBackground");
-
     backgroundChangeWord.innerText = mainContainerBackground.classList.contains(
       "mainBackground"
     )
       ? "jasne"
       : "ciemne";
-  });
+   };
+
+  const init = () => {
+    let backgroundButton = document.querySelector(".backgroundButton");
+    backgroundButton.addEventListener("click", onBackgroundChangeButton);
+  };
+
+   init();
 }
